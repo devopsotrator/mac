00d5658036b139d904bea2a13acfec94843658c0
devopsotrator <doomed@imp.to>
2019-04-30 05:19:10 +0300

    poehali

---
+++ runme.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+set -e
+
+# работаю на нормальном макбуке 2015
+# Комментарии на русском зыке
+# it works
+# no bullshit
+
+# прочитай уже мануал как пользоваться мануалом, я тебя умоляю
+# ты мне спасибо скажешь, брат.
+
+man man | cat > .profile
+
+# если хочешь сразу работать под проксей, 
+# не жлобись, купи себе vps за $5
+# и наслаждаешься чем угодно через 
+# удобную, зашифрованную и главное
+# доступную тебе под одеялом персональную
+# socks5://127.0.0.1:8841
+
+export foo.spam.bar="127.0.0.1"
+ssh -D 8841 root@foo.spam.bar -f -N
+
+# southpark 1080p без регистрации и смс
+# готов seed нормальные фильмыи кникжки на рутреке 
+# commits/issues дам аккаунт
+
+# тебя есть макбук 
+# МАКБУК -- ЕДИНСТВЕННОЕ УСЛОВИЕ
+# ЧТО ТЫ ВЕВЕЗЕШЬ.
+# и это интро конечно, да
+
+# что мы имеем:
+# 	- вряд лм сауспарк уже посмотрен, 
+# 	- работать на было вчер как обычно где-то месяц назад
+
+# 	0) сутки на просмотр
+# 	1) не выебываешься. ставишь brew. 
+
+/usr/bin/ruby \
+	-e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# сразу ставь сублиме текст и начинай туда сублемеровать 
+
+brew cask install sublime-text
+
+# 	2) бегло читаешь https://github.com/drduh/macOS-Security-and-Privacy-Guide
+#			пишет по делу, но brew устанавливает как долбаеб, ставь как я
+
+# тут я прошу у тебя права суперпользователя
+# если ты согласишься -- ничего плохого не случится, никто не умрет
+# но от моих настроек ты можешь немного... растеряться.
+# там расхожее говно с гитхаба, но ты разок лучше прочитай.
+[ -f ./osx.sh ]
+	source os.x 
+
+#	3) пароли храни в макосовской хранилке, НО
+# разбирай на досуге
+wget --mirror --no-parent https://www.romanzolotarev.com/bin/
+
+# учись пользоваться системным фильтром пакетов
+man pf.conf
+
+# p.s. если ты мой студент, советую начать осваивать
+man vi
+
+
+echo ~/bin creted
+echo  ~/bin/pass chkeck700 
+
+# in case of openbsd user
+ftp -o "/tmp/pass.sh"
+
+# you ginne be fine ;)
+
+# for very lazy
+libyubikey
+yubikey-personalization
+
+# JUST GIMME THAT FUCKING 64 SYMBOL PASSWORD ALREADY
+brew cask install "homebrew/cask-drivers/yubico-yubikey-personalization-gui"
+
+#!/bin/sh
+
+set -e
+
+# just wannabe one of all those omg-you-work-for-jigsaw bitches.
+# keep scrolling
