e42499b9989e49b55b62f04035bd32ab1f9cb7da
devopsotrator <devopsotrator@imp.to>
2019-06-21 16:44:55 +0300

    fixed

--- runme.sh
+++ runme.sh
@@ -2,71 +2,46 @@
 set -e
 
 # работаю на нормальном макбуке 2015
-# Комментарии на русском зыке
-# it works
-# no bullshit
+# Комментарии на русском ззке
 
-# прочитай уже мануал как пользоваться мануалом, я тебя умоляю
-# ты мне спасибо скажешь, брат.
+# прочитай мануал как пользоваться мануалом
 
 man man | cat > .profile
 
-# если хочешь сразу работать под проксей, 
-# не жлобись, купи себе vps за $5
-# и наслаждаешься чем угодно через 
-# удобную, зашифрованную и главное
-# доступную тебе под одеялом персональную
+# если хочешь сразу работать под проксей,
+
 # socks5://127.0.0.1:8841
 
 export foo.spam.bar="127.0.0.1"
 ssh -D 8841 root@foo.spam.bar -f -N
 
-# southpark 1080p без регистрации и смс
-# готов seed нормальные фильмыи кникжки на рутреке 
-# commits/issues дам аккаунт
-
-# тебя есть макбук 
+# утебя есть макбук
 # МАКБУК -- ЕДИНСТВЕННОЕ УСЛОВИЕ
 # ЧТО ТЫ ВЕВЕЗЕШЬ.
-# и это интро конечно, да
 
-# что мы имеем:
-# 	- вряд лм сауспарк уже посмотрен, 
-# 	- работать на было вчер как обычно где-то месяц назад
-
-# 	0) сутки на просмотр
-# 	1) не выебываешься. ставишь brew. 
+# ставишь brew.
 
 /usr/bin/ruby \
 	-e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# сразу ставь сублиме текст и начинай туда сублемеровать 
-
 brew cask install sublime-text
 
-# 	2) бегло читаешь https://github.com/drduh/macOS-Security-and-Privacy-Guide
-#			пишет по делу, но brew устанавливает как долбаеб, ставь как я
+# бегло читаешь https://github.com/drduh/macOS-Security-and-Privacy-Guide
 
 # тут я прошу у тебя права суперпользователя
 # если ты согласишься -- ничего плохого не случится, никто не умрет
 # но от моих настроек ты можешь немного... растеряться.
-# там расхожее говно с гитхаба, но ты разок лучше прочитай.
-[ -f ./osx.sh ]
-	source os.x 
+# там расхожее с гитхаба, но ты разок лучше прочитай.
 
-#	3) пароли храни в макосовской хранилке, НО
-# разбирай на досуге
-wget --mirror --no-parent https://www.romanzolotarev.com/bin/
+[ -f ./osx.sh ]
+	source os.x
+
+#	3) пароли храни в макосовской хранилке
 
 # учись пользоваться системным фильтром пакетов
 man pf.conf
 
-# p.s. если ты мой студент, советую начать осваивать
-man vi
-
-
 echo ~/bin creted
-echo  ~/bin/pass chkeck700 
+echo  ~/bin/pass chkeck700
 
 # in case of openbsd user
 ftp -o "/tmp/pass.sh"
@@ -79,10 +54,3 @@ yubikey-personalization
 
 # JUST GIMME THAT FUCKING 64 SYMBOL PASSWORD ALREADY
 brew cask install "homebrew/cask-drivers/yubico-yubikey-personalization-gui"
-
-#!/bin/sh
-
-set -e
-
-# just wannabe one of all those omg-you-work-for-jigsaw bitches.
-# keep scrolling
